b6ece6e07048fdc22e8584dbc31655e9aaac2f6f
Herman Wong <hwong@hermanwong.net>
2022-06-11 21:42:39 +0000

    Test terragrunt dependency.

--- terraform/modules/vsphere/windows/main.tf
+++ terraform/modules/vsphere/windows/main.tf
@@ -3,5 +3,5 @@
 # Fetch template vm data
 data "vsphere_virtual_machine" "template" {
   name          = "${var.vsphere_template_name}"
-  datacenter_id = "${data.terraform_remote_state.base.outputs.dc_id}"
+  datacenter_id = "${var.dc_id}"
 }
