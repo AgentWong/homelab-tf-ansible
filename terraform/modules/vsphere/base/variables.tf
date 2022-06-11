c751bc305603ac6a5f1ae2031dcf470ebf9ea002
Herman Wong <hwong@hermanwong.net>
2022-06-11 02:28:52 +0000

    Remove more vars.

--- terraform/modules/vsphere/base/variables.tf
+++ terraform/modules/vsphere/base/variables.tf
@@ -1,6 +1,3 @@
-variable "vsphere_user" {}
-variable "vsphere_password" {}
-variable "vsphere_server" {}
 variable "vsphere_dc_name" {}
 variable "vsphere_datastore" {}
 variable "vsphere_compute_cluster" {}
