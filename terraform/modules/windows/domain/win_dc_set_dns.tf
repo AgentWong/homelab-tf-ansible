a7c3fcee69a7e9150fa464f6a03b47c9497c4748
Herman Wong <hwong@hermanwong.net>
2022-06-15 01:22:25 +0000

    update ansible_user

--- terraform/modules/windows/domain/win_dc_set_dns.tf
+++ terraform/modules/windows/domain/win_dc_set_dns.tf
@@ -6,7 +6,7 @@ resource "null_resource" "set_dc_dns" {
     command = templatefile("${var.template_file}", { 
       sleep = "",
       change_dir = var.change_dir, 
-      ansible_user = "${var.domain_admin_user}@EDEN.LOCAL"
+      ansible_user = "ansible_user=${var.domain_admin_user}@EDEN.LOCAL"
       password = nonsensitive(data.vault_generic_secret.password.data["password"]),
       extra_args = "pdc_hostname=${var.pdc_name}.${var.join_domain} rdc_hostname=${var.rdc_name}.${var.join_domain}",
       ansible_playbook = var.dc_dns_ansible_playbook
